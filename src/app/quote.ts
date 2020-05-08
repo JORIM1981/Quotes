98172118e838248f81fe76e7b689bc4e214500cb
JORIM1981 <okongo.midumbi.opondo@gmail.com>
2020-05-08 15:19:50 +0300

    add changes to quote.ts

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -3,8 +3,8 @@ export class Quote {
     constructor(
         public text: string,
         public author: string,
-        public poster: string,
-        public posttime: Date,
+        public submitter: string,
+        public completeDate: Date,
         public upvotes: number,
         public downvotes: number
       ) {  }
