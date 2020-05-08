6855f4aa105507307ea758e8afc1b8b28a1f2f90
JORIM1981 <okongo.midumbi.opondo@gmail.com>
2020-05-08 14:31:50 +0300

    add import formsmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
@@ -14,7 +14,8 @@ import { QuoteDetailsComponent } from './quote-details/quote-details.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
